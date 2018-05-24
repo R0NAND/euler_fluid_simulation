33b8ea180d0330a8d967ce41e09f3c9260c1aa71
Ronan <rjtunney@edu.uwaterloo.ca>
2018-05-24 12:04:21 -0400

    Initial commit

---
+++ Object Oriented Fluid Simulator/Field.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "Field.h"
+
+
+Field::Field()
+{
+}
+
+void Field::set(int xSize, int ySize) {
+	x = xSize;
+	y = ySize;
+	xy = (x + 2) * (y + 2);
+	values = new float[(xSize+2)*(ySize+2)];
+	for (int i = 0; i < xy; i++) {
+		values[i] = 0;
+	}
+}
+
+
+Field::~Field()
+{
+}
